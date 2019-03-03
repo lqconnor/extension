ae0881918720f4aa78776ec3d0ba6cb44f55ee89
lqconnor <connorql@gmail.com>
2019-03-03 00:51:42 -0600

    Debt analysis added

--- fm_income.R
+++ fm_income.R
@@ -15,12 +15,17 @@ lapply(pckgs, FUN = function(x) {
 })
 
 
-fm_inc <- read_csv("../../Data/farmincome_wealthstatisticsdata_november2018.csv") %>%
-  filter(State == "US", 
-         str_detect(VariableDescriptionTotal, "Net farm income$"),
-         Year >= 1950) %>%
-  rename(gdp_df = ChainType_GDP_Deflator) %>%
-  mutate(Income = Amount/gdp_df)
+fm_inc <- read_csv("../../Data/farmincome_wealthstatisticsdata_november2018.csv")
 
+# debt -----------------------------------
+debt <- filter(fm_inc,State == "US", 
+       str_detect(VariableDescriptionTotal, "[Dd]ebt, all, excl. operator dwelling"),
+       VariableDescriptionPart1 == "Farm sector debt",
+       Year >= 2004) %>%
+rename(gdp_df = ChainType_GDP_Deflator) %>%
+mutate(debts = Amount/gdp_df)
+
+
+# plots ----------------------------
 ggplot() +
-  geom_line(data = fm_inc, aes(x=Year, y=Income, group = 1), color = "black")
+  geom_line(data = debt, aes(x=Year, y=debts, group = 1), color = "black")
