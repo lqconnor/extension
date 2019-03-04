d393908a49695409c397fd3048f1d78a3b875ce0
lqconnor <connorql@gmail.com>
2019-03-04 01:43:35 -0600

    Drought analysis added

--- fm_income.R
+++ fm_income.R
@@ -21,11 +21,31 @@ fm_inc <- read_csv("../../Data/farmincome_wealthstatisticsdata_november2018.csv"
 debt <- filter(fm_inc,State == "US", 
        str_detect(VariableDescriptionTotal, "[Dd]ebt, all, excl. operator dwelling"),
        VariableDescriptionPart1 == "Farm sector debt",
-       Year >= 2004) %>%
+       Year >= 1985 & Year <= 2016) %>%
 rename(gdp_df = ChainType_GDP_Deflator) %>%
 mutate(debts = Amount/gdp_df)
+
+# Modified Palmer Drought Index Data (PMDI) -----------------------------------------------------------------------------------
+pmdi_files <- c(2016:1985)
+
+# read files into dataframe list
+pdmi <- pmdi_files %>%
+  map(~read_csv(str_c("../../Data/palmer/pmdi_", . ,".txt")))
+
+# Take dataframe list and concatenate by row
+pdmi <- bind_rows(pdmi) %>%
+  select(STATEFP, COUNTYFP, state, year, pdmi) %>%
+  group_by(year) %>%
+  summarize(m_pdmi = mean(pdmi)) %>%
+  mutate(drought = as.numeric(m_pdmi < -0.3))
 
 
 # plots ----------------------------
 ggplot() +
-  geom_line(data = debt, aes(x=Year, y=debts, group = 1), color = "black")
+  geom_line(data = debt, aes(x=Year, y=debts, group = 1), color = "black") +
+  geom_line(data = pdmi, aes(x=year, y=m_pdmi*4e6, group = 1), color = "red")
+
+cor(pdmi$m_pdmi,debt$debts)
+
+run <- lm(log(debt$debts) ~ pdmi$drought)
+summary(run)
