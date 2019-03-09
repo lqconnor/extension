6c22821c75614b58e9c2126822538b7dc17d2685
lqconnor <connorql@gmail.com>
2019-03-08 18:24:29 -0600

    all receipts: livestock and crops comparison, Louisiana and US comparison

--- fm_income.R
+++ fm_income.R
@@ -15,26 +15,27 @@ lapply(pckgs, FUN = function(x) {
 })
 
 
-fm_inc <- read_csv("../../Data/farmincome_wealthstatisticsdata_March2019.csv")
+fm_inc <- read_csv("../Data/farmincome_wealthstatisticsdata_March2019.csv")
 
 # Cash Receipts ---------------------------------
+#excludes <- "commodities"
+excludes <- "commodities|livestock|value, crops|oil crops|lint|feed|mohair|hogs|chicken|broilers|wool|cottonseed|sheep"
+
 receipts <- filter(fm_inc,State == "LA", 
                str_detect(VariableDescriptionTotal, "[Rr]eceipts"),
-               str_detect(VariableDescriptionTotal, "corn|soybeans|cotton ,|rice|for sugar|sorghum|wheat"),
+               !str_detect(VariableDescriptionTotal, excludes),
+               #str_detect(VariableDescriptionTotal, "livestock|value, crops"),
+               #str_detect(VariableDescriptionTotal, "corn|soybeans|cotton ,|rice|for sugar|sorghum|wheat"),
                #VariableDescriptionPart1 == "Farm sector debt",
                VariableDescriptionPart2 == "All",
-               Year >= 2014 & Year <= 2017) %>%
+               Year == 2010) %>%
   mutate(year = as.factor(Year)) %>%
   rename(gdp_df = ChainType_GDP_Deflator) %>%
   mutate(debts = Amount/gdp_df)
 
 
 p <- ggplot(receipts, aes(x = VariableDescriptionPart1, y = Amount)) +
-  geom_bar(
-    aes(color = year, fill = year),
-    stat = "identity", position = position_dodge(0.8),
-    width = 0.7
-  ) +
+  geom_bar(stat = "identity") +
   scale_fill_brewer(palette="Blues") +
   scale_colour_brewer(palette="Blues") +
   theme(axis.title.x=element_blank()) +
@@ -42,81 +43,80 @@ p <- ggplot(receipts, aes(x = VariableDescriptionPart1, y = Amount)) +
 
 p
 
-# Expenses --------------------------------------
-expns <- filter(fm_inc,State == "US", 
-               str_detect(VariableDescriptionTotal, "[Ee]xpenses"),
+# Proportion of farm economy -------------------------
+excluded <- "livestock|value, crops|oil crops|lint|feed|mohair|hogs|chicken|broilers|wool|cottonseed|sheep"
+
+nw_rcpt <- filter(fm_inc,State == "US", 
+               str_detect(VariableDescriptionTotal, "[Rr]eceipts"),
+               !str_detect(VariableDescriptionTotal, excluded),
+               #str_detect(VariableDescriptionTotal, "livestock|value, crops"),
                #str_detect(VariableDescriptionTotal, "corn|soybeans|cotton ,|rice|for sugar|sorghum|wheat"),
                #VariableDescriptionPart1 == "Farm sector debt",
                VariableDescriptionPart2 == "All",
-               Year >= 2016) %>%
-  mutate(year = as.factor(Year))
-
-
-# Debt ------------------------------------------
-debt <- filter(fm_inc,State == "US", 
-               str_detect(VariableDescriptionTotal, "[Dd]ebt, all, excl. operator dwelling"),
-               VariableDescriptionPart1 == "Farm sector debt",
-               Year >= 2004 & Year <= 2016) %>%
-  rename(gdp_df = ChainType_GDP_Deflator) %>%
-  mutate(debts = Amount/gdp_df)
-
-# Farm Income -----------------------------------
-income <- filter(fm_inc, str_detect(State, "US|LA"), 
-               str_detect(VariableDescriptionTotal, "Net [Ff]arm income$"),
-               #VariableDescriptionPart1 == "Farm sector debt",
-               Year >= 2012) %>%
+               Year >= 2005) %>%
+  select(Year, State, VariableDescriptionPart1, Amount) %>%
   mutate(year = as.factor(Year)) %>%
-  rename(gdp_df = ChainType_GDP_Deflator) %>%
-  mutate(debts = Amount/gdp_df)
+  spread(VariableDescriptionPart1, Amount)
 
-p <- ggplot(income, aes(x = State, y = log(Amount))) +
-  geom_bar(
-    aes(color = year, fill = year),
-    stat = "identity", position = position_dodge(0.8),
-    width = 0.7
-  ) +
-  scale_fill_brewer(palette="Blues") +
-  scale_colour_brewer(palette="Blues") +
-  theme(axis.title.x=element_blank(),
-       axis.text.x=element_blank(),
-       axis.ticks.x=element_blank())
+ggplot(nw_rcpt) +
+  geom_line(aes(x = Year, y = (`Soybeans`/`All Commodities`)*100), color = "black") +
+  geom_line(aes(x = Year, y = (`Cane for sugar`/`All Commodities`)*100), color = "red") +
+  geom_line(aes(x = Year, y = (`Corn`/`All Commodities`)*100), color = "blue") +
+  geom_line(aes(x = Year, y = (`Poultry/Eggs`/`All Commodities`)*100), color = "purple") +
+  geom_line(aes(x = Year, y = (`Rice`/`All Commodities`)*100), color = "orange") +
+  geom_line(aes(x = Year, y = (`Cotton`/`All Commodities`)*100), color = "brown")
 
-p
 
-ggplot(income, aes(x = year, y = Amount,
-                      group = State,
-                      colour = State)) +
-  geom_line()
+ggplot(nw_rcpt) +
+  geom_line(aes(x = Year, y = `Soybeans`), color = "black") +
+  geom_line(aes(x = Year, y = `Cane for sugar`), color = "red") +
+  geom_line(aes(x = Year, y = `Corn`), color = "blue") +
+  geom_line(aes(x = Year, y = `Poultry/Eggs`), color = "purple") +
+  geom_line(aes(x = Year, y = `Rice`), color = "orange") +
+  geom_line(aes(x = Year, y = `Cotton`), color = "brown")
+
+ggplot(nw_rcpt) +
+  geom_line(aes(x = Year, y = `Soybeans`), color = "black") +
+  geom_line(aes(x = Year, y = `Cane for sugar`), color = "red") +
+  geom_line(aes(x = Year, y = `Corn`), color = "blue") +
+  geom_line(aes(x = Year, y = `Poultry/Eggs`), color = "purple") +
+  geom_line(aes(x = Year, y = `Rice`), color = "orange") +
+  geom_line(aes(x = Year, y = `Cotton`), color = "brown")
+
+# Proportion of farm economy -------------------------
+# Livestock and Crops --------------------
+all_rcpt <- filter(fm_inc,State == "US", 
+                  str_detect(VariableDescriptionTotal, "[Rr]eceipts"),
+                  str_detect(VariableDescriptionTotal, ", crops|livestock"),
+                  VariableDescriptionPart2 == "All",
+                  Year >= 2005) %>%
+  select(Year, State, VariableDescriptionPart1, Amount) %>%
+  spread(VariableDescriptionPart1, Amount)
+
+ggplot(all_rcpt) +
+  geom_line(aes(x = Year, y = `Crops`), color = "black") +
+  geom_line(aes(x = Year, y = `Animals and products`), color = "red")
+
+# Proportion of national farm economy -------------------------
+US_rcpt <- filter(fm_inc, str_detect(State, "LA|US"), 
+                  str_detect(VariableDescriptionTotal, "[Rr]eceipts"),
+                  str_detect(VariableDescriptionTotal, "commodities"),
+                  VariableDescriptionPart2 == "All",
+                  Year >= 2005) %>%
+  select(Year, State, VariableDescriptionPart1, Amount) %>%
+  mutate(year = as.factor(Year)) %>%
+  spread(State, Amount)
+
+ggplot(US_rcpt) +
+  geom_line(aes(x = Year, y = (LA/US)*100), color = "black")
+
+ggplot(US_rcpt) +
+  geom_line(aes(x = Year, y = US*100), color = "black") +
+  geom_line(aes(x = Year, y = LA*100), color = "blue") 
 
 ggplot() +
-  geom_line(data = debt, aes(x=Year, y=Amount, group = 1), color = "black")
-geom_line(data = debt, aes(x=Year, y=Amount, group = 1), color = "black")
-
-# Pie Charts -----------------------------------
-
-# Create a basic bar
-receipts <- filter(fm_inc,State == "LA", 
-                   str_detect(VariableDescriptionTotal, "[Rr]eceipts"),
-                   #str_detect(VariableDescriptionTotal, "corn|soybeans|cotton ,|rice|for sugar|sorghum|wheat"),
-                   #VariableDescriptionPart1 == "Farm sector debt",
-                   VariableDescriptionPart2 == "All",
-                   Year <= 2017)
-
-pie = ggplot(insnc, aes(x="", y=s_psld, fill=inspln_abbrv)) + geom_bar(stat="identity", width=1)
-
-# Convert to pie (polar coordinates) and add labels
-pie = pie + coord_polar("y", start=0) + geom_text(aes(label = paste0(prop, "%")), position = position_stack(vjust = 0.5), color = "white")
-
-# Add color scale (hex colors)
-pie = pie + scale_fill_manual(values=c("#55DDE0", "#33658A", "#2F4858", "#F6AE2D", "#F26419", "#999999")) 
-
-# Remove labels and add title
-pie = pie + labs(x = NULL, y = NULL, fill = NULL, title = "Statewide Insurance Plan Use")
-
-# Tidy up the theme
-pie + theme_classic() + theme(axis.line = element_blank(),
-                              axis.text = element_blank(),
-                              axis.ticks = element_blank(),
-                              plot.title = element_text(hjust = 0.5, color = "#666666"))
-
-
+  geom_line(data = US_rcpt, aes(x = Year, y = log(LA)), color = "black") +
+  geom_line(data = US_rcpt, aes(x = Year, y = log(US)/1.3), color = "red") +
+  geom_line(data = nw_rcpt, aes(x = Year, y = log(`Corn`)*1.1), color = "blue") +
+  geom_line(data = nw_rcpt, aes(x = Year, y = log(`Poultry/Eggs`)*1.1), color = "purple") +
+  geom_line(data = nw_rcpt, aes(x = Year, y = log(`Soybeans`)*1.1), color = "brown")
