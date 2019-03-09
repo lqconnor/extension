41ea43ab753e39a0301c717d4f69e0dfbc6a58ac
lqconnor <connorql@gmail.com>
2019-03-08 18:31:44 -0600

    national farm income

--- fm_income.R
+++ fm_income.R
@@ -15,29 +15,15 @@ lapply(pckgs, FUN = function(x) {
 })
 
 
-fm_inc <- read_csv("../../Data/farmincome_wealthstatisticsdata_november2018.csv")
+fm_inc <- read_csv("../Data/farmincome_wealthstatisticsdata_march2019.csv")
 
 # debt -----------------------------------
-debt <- filter(fm_inc,State == "US", 
-       str_detect(VariableDescriptionTotal, "[Dd]ebt, all, excl. operator dwelling"),
-       VariableDescriptionPart1 == "Farm sector debt",
-       Year >= 2004 & Year <= 2016) %>%
+income <- filter(fm_inc,State == "US", 
+       str_detect(VariableDescriptionTotal, "Net cash income"),
+       #VariableDescriptionPart1 == "Farm sector debt",
+       Year >= 2010) %>%
 rename(gdp_df = ChainType_GDP_Deflator) %>%
 mutate(debts = Amount/gdp_df)
-
-# Modified Palmer Drought Index Data (PMDI) -----------------------------------------------------------------------------------
-pmdi_files <- c(2016:2004)
-
-# read files into dataframe list
-pdmi <- pmdi_files %>%
-  map(~read_csv(str_c("../../Data/palmer/pmdi_", . ,".txt")))
-
-# Take dataframe list and concatenate by row
-pdmi <- bind_rows(pdmi) %>%
-  select(STATEFP, COUNTYFP, state, year, pdmi) %>%
-  group_by(year) %>%
-  summarize(m_pdmi = mean(pdmi)) %>%
-  mutate(drought = as.numeric(m_pdmi < -0.3))
 
 
 # plots ----------------------------
